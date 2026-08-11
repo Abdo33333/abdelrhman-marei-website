7cee37b3c196ae7d6d70761688b2bb6e9f1cb7c8
Abdo33333 <abdelrhmantreader2@gmail.com>
2026-08-11 21:16:50 +0300

    Initialize client site enhancements

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+import { initSiteEnhancements } from "./site-enhancements";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
@@ -11,6 +12,10 @@ export const getRouter = () => {
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   });
+
+  if (typeof window !== "undefined") {
+    window.setTimeout(() => initSiteEnhancements(), 0);
+  }
 
   return router;
 };
